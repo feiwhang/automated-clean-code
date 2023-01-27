df9ee70c32f0cd4f67790926a7eaed905a3a796d
feiwhang <feiwhang@gmail.com>
2023-01-27 15:12:03 +0700

    feat: add multiply method

--- src/automated_clean_code/__init__.py
+++ src/automated_clean_code/__init__.py
@@ -9,3 +9,16 @@ def add_numbers(x: int, y: int) -> int:
       (int). The sum of the two numbers.
     """
     return x + y
+
+
+def multiply_numbers(x: int, y: int) -> int:
+    """Multiply two numbers together.
+
+    Args:
+      x (int): a number
+      y (int): a number
+
+    Returns:
+      (int). The product of the two numbers.
+    """
+    return x * y
